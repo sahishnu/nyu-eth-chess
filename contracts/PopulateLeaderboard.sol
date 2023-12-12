3a8ba9e89a6148dfcaa945a8bc6b5afa431a63d3
Lawrence Lim <ll4715@nyu.edu>
2023-12-12 04:19:54 -0500

    fixed syntax errors and renamed files
    two failing tests remain

--- contracts/PopulateLeaderboard.sol
+++ contracts/PopulateLeaderboard.sol
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+import "./ChessLeaderboard.sol";
+
 contract PopulateLeaderboard is IChessContract {
     ChessLeaderboard public leaderboard;
 
